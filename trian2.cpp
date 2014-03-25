173dd668bc38674516d00941abeeb3b50d6c15aa
Gilberto <tripeggg@gmail.com>
2014-03-24 22:56:08 -0600

    Create trian2.cpp
    Triangulo
    ****
    ***
    **
    *

---
+++ trian2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+int main(){
+    int a=0;
+    scanf("%d",&a);
+    int n;
+    for(int i=0;i<a;i++){
+            for(int j=i;j<a;j++){
+                    printf("*");
+                    }
+                    printf("\n");
+                    }
+                    system("PAUSE");
+    }
