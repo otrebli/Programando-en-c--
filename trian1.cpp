787fed7f0f978fc3998d616931697db831465cc8
Gilberto <tripeggg@gmail.com>
2014-03-24 22:55:02 -0600

    Create trian1.cpp
    Dibujar un triangulo 
    *
    **
    ***
    ****

---
+++ trian1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream.h>
+int main(){
+    int a, i ,j;
+    scanf("%d",&a);
+    for(i=0;i<a;i++){
+                     for(j=0;j<=i;j++){
+                                       printf("*");
+                                       }
+                                       printf("\n");
+                     }
+                     system("PAUSE");
+    }
