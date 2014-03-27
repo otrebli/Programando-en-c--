95e22442b6e2e53452bfbd1ba947e233eb35bc27
Gilberto <tripeggg@gmail.com>
2014-03-26 19:53:25 -0600

    Create prueba.c
    Programa en c para ver la cantidad de bytes en memoria que ocupa cada tipo de dato.

---
+++ prueba.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+char a;
+int n;
+long l;
+float f;
+double d;
+printf("%d\n",sizeof(a));
+printf("%d\n",sizeof(n));
+printf("%d\n",sizeof(l));
+printf("%d\n",sizeof(f));
+printf("%d\n",sizeof(d));
+return 0;
+}
